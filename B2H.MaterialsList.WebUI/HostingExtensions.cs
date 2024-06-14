0ae70fd18482f5cd2b4cb628840fd9de940b857b
Chaos Sherill <64668105+Lynax1P@users.noreply.github.com>
2024-06-14 19:47:09 +0300

    save

--- B2H.MaterialsList.WebUI/HostingExtensions.cs
+++ B2H.MaterialsList.WebUI/HostingExtensions.cs
@@ -73,9 +73,8 @@ namespace B2H.MaterialsList.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            //app.UseHttpsRedirection();
             app.UseStaticFiles();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
