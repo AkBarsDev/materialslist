cfd309aaf3747aa269040fd1eb2c40b57d5518fb
kopysovae <copysova.jenia@gmail.com>
2024-06-21 12:18:52 +0300

    Добавлена База знаний

---
+++ B2H.MaterialsList.WebUI/Controllers/KnowledgeBaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaterialsList.Frontend.Controllers
+{
+	public class KnowledgeBaseController : Controller
+	{
+		public async Task<IActionResult> KnowledgeBase()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> TopicPage()
+		{
+			return View();
+		}
+	}
+}
---
+++ B2H.MaterialsList.WebUI/wwwroot/js/know-category.js
@@ -0,0 +1,17 @@
+﻿document.getElementById('category-button').addEventListener('click', function () {
+	var categoryList = document.getElementById('category-list');
+	categoryList.classList.toggle('active');
+});
+
+document.querySelectorAll('.category-item').forEach(function (item) {
+	item.addEventListener('click', function () {
+		var category = this.getAttribute('data-category');
+		document.querySelectorAll('.base-board-card').forEach(function (card) {
+			if (card.id === category) {
+				card.style.display = 'block';
+			} else {
+				card.style.display = 'none';
+			}
+		});
+	});
+});
