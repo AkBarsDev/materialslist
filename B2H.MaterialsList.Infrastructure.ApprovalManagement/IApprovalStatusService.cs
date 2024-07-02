ef2aeba52257feb53d5ab8b5b2d5d245fc0d27a1
Chaos Sherill <64668105+Lynax1P@users.noreply.github.com>
2024-07-03 01:14:09 +0300

    Create IApprovalStatusService.cs

---
+++ B2H.MaterialsList.Infrastructure.ApprovalManagement/IApprovalStatusService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2H.MaterialsList.Infrastructure.ApprovalManagement
+{
+	internal interface IApprovalStatusService
+	{
+	}
+}
