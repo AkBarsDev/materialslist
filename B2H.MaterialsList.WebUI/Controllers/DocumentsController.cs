5d39738a47afc9f5609b39b8775f364d93330b4a
kopysovae <copysova.jenia@gmail.com>
2024-07-08 18:06:34 +0300

    Documents раздел ui

---
+++ B2H.MaterialsList.WebUI/Controllers/DocumentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B2H.MaterialsList.WebUI.Controllers
+{
+	public class DocumentsController : Controller
+	{
+		public IActionResult Documents()
+		{
+			return View();
+		}
+	}
+}
